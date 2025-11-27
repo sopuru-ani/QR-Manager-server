613aef7ca511ec01e3cbf7ba5ca9bd1fc5b6ead9
sopuru-ani <136132869+sopuru-ani@users.noreply.github.com>
2025-11-27 02:05:18 -0500

    Update CORS configuration for production

--- app.js
+++ app.js
@@ -21,10 +21,11 @@ const secretKey = process.env.JWT_SECRET;
 const exp = process.env.JWT_LIMIT;
 
 // Middleware
-app.use(cors(({
-    origin: "http://localhost:5173", // NOT "*"
-    credentials: true
-})));
+app.use(cors({
+  origin: "https://qr-manager-beige.vercel.app",
+  methods: ["GET","POST","PUT","DELETE"],
+  credentials: true
+}));
 app.use(express.json());
 app.use(cookieParser());
 
